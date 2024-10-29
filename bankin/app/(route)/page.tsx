f53f6f81ab744e3e91a347003ee4ebb196fba805
confidenceaffang <coaffa01@wsc.edu>
2024-10-29 17:36:36 -0500

    started the header component

--- bankin/app/(route)/page.tsx
+++ bankin/app/(route)/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
 
 const Home = () => {
+  const loggedIn = {firstname: 'Confidence'};
+
   return (
-    <div>
-      DASHBOARD
+   <section className="home">
+    <div className="home-content">
+    <header className="home-header">
+        <HeaderBox 
+        type ="greeting"
+        title ="Welcome"
+        user ={loggedIn?.firstname || "Guest"}
+        subtext = "Access and manage your account efficitently"/>
+    </header>
     </div>
+   </section>
   )
 }
+
 export default Home;
