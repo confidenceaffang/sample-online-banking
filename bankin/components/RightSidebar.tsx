5a9005e08c8ee9386ba76cbbb0c1cc099f22511f
confidenceaffang <coaffa01@wsc.edu>
2024-11-01 18:50:42 -0500

    creating and designing bank card with static data of 2 banks

--- bankin/components/RightSidebar.tsx
+++ bankin/components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import BankCard from './BankCard'
 
 const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
   return (
@@ -38,10 +39,18 @@ const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
             {banks?.length>0 &&(
                 <div className='relative flex flex-1 flex-col items-center justify-center gap-4'>
                     <div className='relative z-10 '>
+                        <BankCard key={banks[0].$id}
+                        accounts = {banks[0]}
+                        userName ={`${user.firstName} ${user.lastName}`}
+                        showBalance ={false} />
                         CARD 1  
                     </div>
                     {banks[1] && (
                         <div className='absolute right-0 top-8 z-0 w-[90%]'>
+                             <BankCard key={banks[1].$id}
+                        accounts = {banks[1]}
+                        userName ={`${user.firstName} ${user.lastName}`}
+                        showBalance ={false} />
                             CARD 2
                         </div>
                     )}
