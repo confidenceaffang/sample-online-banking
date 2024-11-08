b1b761dbf6e9686124aa6cbc08d3dc2c05eb8b4a
confidenceaffang <coaffa01@wsc.edu>
2024-11-08 16:47:28 -0600

    added the routes for sidebar links

--- bankin/components/TotalBalanceBox.tsx
+++ bankin/components/TotalBalanceBox.tsx
@@ -21,8 +21,6 @@ const TotalBalanceBox = ({
                 </p>
                 <p className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter amount = {totalCurrentBalance}/>
-                    
-                   
                 </p>
 
             </div>
